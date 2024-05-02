83fc056f39d828edd6e877bd62d9cf2038dee0ff
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-02 17:43:41 +0530

    Update SharedFiles.js

--- frontend/src/components/SharedFiles.js
+++ frontend/src/components/SharedFiles.js
@@ -22,7 +22,7 @@ const SharedFiles = ({ onClose, title, content, buttons, profile_id }) => {
     return (
       <div style={{maxHeight: "70vh", overflowY: "auto"}}>
         <div>
-             <h2>Uploaded Files</h2>
+             <h2 style={{marginLeft: "200px"}}>Uploaded Files</h2>
             <File
             />
         </div>
