71073c908429a740e066be9850911d8e35f2802c
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-09 12:36:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,8 @@ app.use(cors(
             "http://localhost:80",
             "https://olll-cha-ts-backend.6o1yzt.easypanel.host:9000",
             "https://olll-cha-ts-backend.6o1yzt.easypanel.host",
-            "https://olll-cha-ts-frontend.6o1yzt.easypanel.host:80",
-            "https://olll-cha-ts-frontend.6o1yzt.easypanel.host"
+            "https://any-chats-front-end-chat.yi9ne2.easypanel.host:80",
+            "https://any-chats-front-end-chat.yi9ne2.easypanel.host"
 
             
           ],
